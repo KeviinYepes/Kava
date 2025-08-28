29499c548b58652682e54adf399e62f95f98fbb9
KeviinYepes <keyv2412@gmail.com>
2025-08-27 20:57:21 -0500

    update login and controller

--- models/user_model.py
+++ models/user_model.py
@@ -1,6 +1,9 @@
 class User:
-    def __init__(self, id, username, password, fullname, document,role):
-        self.id = id 
+    #El método __init__ es el constructor de la clase
+    #Se ejecuta cada vez que creamos un nuevo usuario
+   class User:
+    def __init__(self, id, username, password, role, fullname=None, document=None):
+        self.id = id
         self.username = username
         self.password = password
         self.fullname = fullname
